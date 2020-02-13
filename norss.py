7304a5881c962d1229f2d106407895eb44912aef
Eliseo Do Brito <eliseodb@gmail.com>
2020-02-13 10:07:35 -0300

    Add get_link_list method

--- norss.py
+++ norss.py
@@ -1,6 +1,34 @@
 import requests, textwrap, subprocess, sys
 from bs4 import BeautifulSoup
 
+def get_link_list():
+	url = 'https://www.lmneuquen.com'
+
+	page = requests.get(url)
+
+	if page.status_code != 200:
+		return False
+
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	# Save in a file
+	file = open('__norss_links.txt', 'w+')
+
+	n = 1
+	links = ''
+
+	for link in soup.select('.title a'):
+		print(str(n) + '|' + link.getText())
+		links += str(n) + '|' + link.getText()
+		file.write(str(n) + '|' + link['href'] + '|' + link.getText() + '\n')
+
+		n = n + 1
+
+	file.close()
+
+	return links
+
+
 def get_article(url):
 	page = requests.get(url)
 
